e2a843b460a0897e0fbd5c95c2cbe37115789e58
hong0ki <1791653823@qq.com>
2018-04-03 09:38:55 +0800

    恩恩

---
+++ src/com/lzh/main/Start.java
@@ -0,0 +1,16 @@
+/**
+ * @Package: com.lzh.main
+ * @author: THINK 
+ * @date: 2018年4月3日 上午9:37:15 
+ */
+package com.lzh.main;
+
+/**
+ * @ClassName: Start
+ * @Description: 程序入口
+ * @author THINK
+ * @date: 2018年4月3日 上午9:37:15 
+ */
+public class Start {
+
+}
